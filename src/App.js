ca74e88a90c6bf824467a2f90c4390635bbd2690
Daniel <danielrs9705@gmail.com>
2019-01-10 23:51:11 -0400

    Remove all the react presentation in the home of the App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import './Resume.css';
+import MyNavigationBar from './NavBar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <MyNavigationBar nombre="Daniel Rodríguez"/>
         Hello I am Daniel and This is my Portfolio
       </div>
     );
