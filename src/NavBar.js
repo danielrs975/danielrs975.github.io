d7bb6cbf7d38af4ddc407007b390c29df54fb52b
Daniel <danielrs9705@gmail.com>
2019-01-12 23:33:46 -0400

    Finished the navbar for the portfolio

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,42 +3,39 @@ This script implements the
 navigation bar component
  */
 import React from 'react';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 function MyNavBar(props){
     const skills = props.skills
     const skillsList = skills.map((skill, index) => 
-        <a key={index} className="dropdown-item" href="#">{skill}</a>
+        <MenuItem key={index} eventKey={index}>{skill}</MenuItem>
     );
     const navStyle = {
         color: '#dce3e3',
         fontSize: 25
     }
     return (
-        <nav className="background navbar navbar-expand-lg navbar-light" style={{backgroundColor: '#2e2e2e'}}>
-            <div className="my-name">
-                <span className="mb-0 h1" style={navStyle}>{props.nombre}</span>
-            </div>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <div className="option-menu">
-                    <ul className="navbar-nav" style={navStyle}>
-                        <li className="nav-item active"> 
-                            <a className="nav-link nav-color" href="#">Profile</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle nav-color" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-expanded="false">
-                                Skills
-                            </a>
-                            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                {skillsList}
-                            </div>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link nav-color" href="#">Projects</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <Navbar inverse fixedTop collapseOnSelect className="font">
+            <Navbar.Header>
+                <Navbar.Brand>
+                    <span className="my-name">Daniel Rodríguez</span>
+                </Navbar.Brand>
+            <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+                <Nav pullRight>
+                    <NavItem eventKey={1} href="#" className="menu">
+                        Profile
+                    </NavItem>
+                    <NavDropdown eventKey={2} title="Skills" id="basic-nav-dropdown" className="menu">
+                        {skillsList}
+                    </NavDropdown>
+                    <NavItem eventKey={3} href="#" className="menu">
+                        Projects
+                    </NavItem>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 }
 
