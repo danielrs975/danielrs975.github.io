7e4ad6544cfde25b944ef96124663992ce5c3c6a
Daniel <danielrs9705@gmail.com>
2019-01-14 19:31:31 -0400

    Se activa la animacion del mensaje de welcome

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,47 +1,48 @@
 import React from 'react';
-import posed from 'react-pose';
+import posed, { PoseGroup } from 'react-pose';
 import { Jumbotron, Grid, Row, Col, Image } from 'react-bootstrap';
 import foto from './foto.png'
 
 const Box = posed.div({
-    enter: { 
+    enter: {
         opacity: 1,
-        delay: 300,
+        delay: 550,
         transition: {
             y: {type: 'spring', stiffness: 1000, damping: 15 },
-            default: {duration: 300}
+            default: {duration: 1000}
         }
     },
     exit: {
-        y: 50,
         opacity: 0,
         transition: { duration: 150 }
     }
 });
 
 class Welcome extends React.Component {
-    state = { isVisible: false };
+    state = { isVisible: false }
 
     componentDidMount(){
-        this.setState({isVisible: false});
+        this.setState({ isVisible: true });
     }
-
-    comp
 
     render(){
         const { isVisible } = this.state;
         return (
             <Jumbotron bsClass="jumbotron welcome-panel">
                 <Grid>
-                    <Box pose={isVisible ? 'exit' : 'enter'} > 
-                        <Row key="row">
-                            <Col xs={6} md={4}>
-                                <Image src={foto} circle bsClass="photo"/>
-                            </Col>
-                        </Row>
-                        <h1 key="welcome">Welcome!</h1>
-                        <h2 key="presentation">My name is Daniel and this is my portfolio</h2>
-                    </Box>
+                    <PoseGroup>
+                        {isVisible && [
+                            <Box key="welcome"> 
+                                <Row>
+                                    <Col xs={6} md={4}>
+                                        <Image src={foto} circle bsClass="photo"/>
+                                    </Col>
+                                </Row>
+                                <h1>Welcome!</h1>
+                                <h2>My name is Daniel and this is my portfolio</h2>
+                            </Box>,
+                        ]}
+                    </PoseGroup>
                 </Grid>
             </Jumbotron>
         );
