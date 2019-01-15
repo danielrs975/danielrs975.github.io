6861cafdecf7ad1ce61ff51422f901780b091b59
Daniel <danielrs9705@gmail.com>
2019-01-14 22:30:10 -0400

    Implement the skills view

---
+++ src/Skills.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProgrammingLanguages from './ProgrammingLanguages';
+
+function Skills(){
+    return (
+        <div style={{backgroundColor: '#676767', color: '#c8cfcf'}} className="font">
+            <h1>Skills</h1>
+            <ProgrammingLanguages />
+        </div>
+    );
+}
+
+export default Skills;
