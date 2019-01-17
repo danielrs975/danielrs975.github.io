eb2abbe01836dc332a76b1c998664e68a84cb40a
Daniel <danielrs9705@gmail.com>
2019-01-16 21:41:57 -0400

    Remove skills label

--- src/Skills.js
+++ src/Skills.js
@@ -4,7 +4,6 @@ import ProgrammingLanguages from './ProgrammingLanguages';
 function Skills(){
     return (
         <div style={{backgroundColor: '#676767', color: '#c8cfcf'}} className="font">
-            <h1>Skills</h1>
             <ProgrammingLanguages />
         </div>
     );
