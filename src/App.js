1b69a4bd076adef329c8343eb64cab10f5a0ed40
Daniel <danielrs9705@gmail.com>
2019-01-21 01:37:18 -0400

    Revierte todo lo hecho

--- src/App.js
+++ src/App.js
@@ -2,17 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 import './Resume.css';
 import MyNavBar from './NavBar';
-import Welcome from './Welcome';
-import Skills from './Skills';
+
 
 class App extends Component {
   render() {
-    const skills = ['Programming Languages', 'Web Development', 'Database', 'Languages']
     return (
       <div className="App">
-        <MyNavBar nombre="Daniel Rodríguez" skills={skills}/>
-        <Welcome />
-        <Skills />
       </div>
     );
   }
