7cd841b7473f0239773ea56c0e4ba511db8f33c9
Daniel <danielrs9705@gmail.com>
2019-01-21 21:02:23 -0400

    Se cambia el nombre del brand

--- src/NavBar.js
+++ src/NavBar.js
@@ -11,7 +11,7 @@ function MyNavBar(props){
             <div id="nav-header">
                 <div className="image-responsive">
                     <img id="sprite-daniel" src={sprite} alt="sprite-daniel"/>
-                    <h2 className="navbar-brand mb-0 h1" style={{paddingBottom: '12px', fontSize: '23px'}}>DRSOFTDEVELOPER</h2>
+                    <h2 className="navbar-brand mb-0 h1" style={{paddingBottom: '12px', fontSize: '23px'}}>DANIEL RODRÍGUEZ</h2>
                 </div>
             </div>
             <div className="collapse navbar-collapse" id="nav-opciones">
