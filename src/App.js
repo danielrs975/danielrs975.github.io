bc9fa72daf5a84597410031abaf6fdfba5574db0
Daniel <danielrs9705@gmail.com>
2020-05-03 16:34:19 +0200

    Reorganization of the styles in the project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import './styles/App.css';
-import './styles/Resume.css';
 import MyNavBar from './components/NavBar';
 import WelcomeMessage from './components/WelcomeMessage';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <MyNavBar />
-        <WelcomeMessage />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<MyNavBar />
+				<WelcomeMessage />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,26 +4,35 @@ navigation bar component
  */
 import React from 'react';
 
-function MyNavBar(props){
-    return (
-        <nav className="navbar fixed-top navbar-expand-lg navbar-light" style={{paddingBottom: '0px'}}>
-            <div id="nav-header">
-                <div className="image-responsive">
-                    <img id="sprite-daniel" src="/images/danielSprite.png" alt="sprite-daniel"/>
-                    <h2 id="name" className="navbar-brand mb-0 h1">DANIEL RODRÍGUEZ</h2>
-                </div>
-            </div>
-            <div className="collapse navbar-collapse" id="nav-opciones">
-                <div className="navbar-nav">
-                    <a className="nav-item nav-link active" href="#welcome-section">Home</a>
-                    <a className="nav-item nav-link" href="#about-me-section">About Me</a>
-                    <a className="nav-item nav-link" href="#portfolio">Projects</a>
-                    <a className="nav-item nav-link" href="#footer">Contact</a>
-                    <a className="nav-item nav-link" href="#menu" id="menu-mobile"><i className="material-icons" style={{fontSize: '28px'}}>menu</i></a>
-                </div>
-            </div>
-        </nav>        
-    );
+function MyNavBar(props) {
+	return (
+		<nav className="navbar fixed-top navbar-expand-lg navbar-light" style={{ paddingBottom: '0px' }}>
+			<div id="nav-header">
+				<div className="image-responsive">
+					<img id="sprite-daniel" src="/images/danielSprite.png" alt="sprite-daniel" />
+					<h2 id="name" className="navbar-brand mb-0 h1">
+						DANIEL RODRÍGUEZ
+					</h2>
+				</div>
+			</div>
+			<div className="collapse navbar-collapse" id="nav-opciones">
+				<div className="navbar-nav">
+					<a className="nav-item nav-link active" href="#welcome-section">
+						Home
+					</a>
+					<a className="nav-item nav-link" href="#about-me-section">
+						About Me
+					</a>
+					<a className="nav-item nav-link" href="#portfolio">
+						Projects
+					</a>
+					<a className="nav-item nav-link" href="#footer">
+						Contact
+					</a>
+				</div>
+			</div>
+		</nav>
+	);
 }
 
 export default MyNavBar;
