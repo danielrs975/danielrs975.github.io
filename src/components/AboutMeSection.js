784810695bb4ca6766ad4ef997f047613ca2f967
Daniel Rodriguez <danielrs9705@gmail.com>
2020-05-06 17:12:03 +0200

    Add some info about me

--- src/components/AboutMeSection.js
+++ src/components/AboutMeSection.js
@@ -4,8 +4,24 @@ const AboutMeSection = () => {
     return (
         <div id="aboutMe" className="section">
             <div className="content-container">
-                <div class="section__header">
+                <div className="section__header">
                     <h1>About Me</h1>
+                </div>
+                <div className="row about-me__content">
+                    <div className="about-me__photo center">
+                        <img src="/images/foto.png" alt="me" />
+                    </div>
+                    <div className="about-me__bio center">
+                       <p>
+                            Today, I am a MSc Student in Computer Science at Télécom SudParis, France.<br/>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod<br/> 
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, <br/>
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br/>
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat<br/> 
+                            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia<br/>
+                            deserunt mollit anim id est laborum.
+                       </p>
+                    </div>
                 </div>
             </div>
         </div>
