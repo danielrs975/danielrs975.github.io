40fa61beba6a47ab7057a753511e7c080f17606c
Daniel <danielrs9705@gmail.com>
2020-05-16 12:33:54 +0200

    Updates

---
+++ precache-manifest.0ee3c6c1160e302ab5052e90018845b3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dbdb2c1c009c5138bbb",
+    "url": "/static/css/main.66248ead.chunk.css"
+  },
+  {
+    "revision": "8dbdb2c1c009c5138bbb",
+    "url": "/static/js/main.8dbdb2c1.chunk.js"
+  },
+  {
+    "revision": "9131d5939264cffab6f0",
+    "url": "/static/css/1.7beba1e2.chunk.css"
+  },
+  {
+    "revision": "9131d5939264cffab6f0",
+    "url": "/static/js/1.9131d593.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f1229c7748d7f76b766443a16fd7630f",
+    "url": "/static/media/foto.f1229c77.png"
+  },
+  {
+    "revision": "7f395c49ca8dbd99954c8c5c414c67bc",
+    "url": "/static/media/joincode.7f395c49.PNG"
+  },
+  {
+    "revision": "22912f4d47fb5b34e55b5eb7827e1c9d",
+    "url": "/static/media/expensifyapp.22912f4d.PNG"
+  },
+  {
+    "revision": "13d1e28e9025521972165d4eba68e7ce",
+    "url": "/index.html"
+  }
+];
