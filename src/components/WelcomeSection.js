d876c523275485e0cf722b8f56811485873b4ea2
Daniel Rodriguez <danielrs9705@gmail.com>
2020-05-31 12:39:23 +0200

    Add a folder with the content of the page

--- src/components/WelcomeSection.js
+++ src/components/WelcomeSection.js
@@ -4,21 +4,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithubSquare, faLinkedin, faTwitterSquare } from '@fortawesome/free-brands-svg-icons';
-
-const translate = {
-    en: {
-        presentation: (<h1>Hi, I'm Daniel <br/> and this is my portfolio</h1>),
-        description: (<h2>MSc student in Computer Science</h2>)
-    },
-    es: {
-        presentation: (<h1>Hola, me llamo Daniel <br/> y este es mi portafolio</h1>),
-        description: (<h2>Estudiante de Master en Ciencias de la Computación</h2>)
-    },
-    fr: {
-        presentation: (<h1>Salut, je m'appelle Daniel <br/> et c'est mon portfolio</h1>),
-        description: (<h2>Étudiant de Master en Science de la Computation</h2>)
-    }
-}
+import translate from '../translations/WelcomeSection';
 
 const WelcomeSection = (props) => {
     return (
@@ -27,11 +13,9 @@ const WelcomeSection = (props) => {
                 <div className="welcome__content">
                     <div className="welcome__message">
                         {translate[props.lang].presentation}
-                        {/* <h1>Hi, I'm Daniel <br/> and this is my portfolio</h1> */}
                     </div>
                     <div className="welcome__currenttitle">
                         {translate[props.lang].description}
-                        {/* <h2>MSc student in Computer Science</h2> */}
                         <div className="row social-medias">
                             <a href="https://github.com/danielrs975" 
                                 target="__blank" 
