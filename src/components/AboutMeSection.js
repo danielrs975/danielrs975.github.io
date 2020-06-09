64b64e58987ae70be1d3be918a55f7b88b687651
Daniel Rodriguez <danielrs9705@gmail.com>
2020-06-09 12:36:16 +0200

    Fetch the information from firebase

--- src/components/AboutMeSection.js
+++ src/components/AboutMeSection.js
@@ -5,45 +5,66 @@ import { faAngular, faReact, faNode, faPython, faHtml5, faSass, faGitAlt } from 
 import SkillBox from './SkillBox';
 
 // My Info
-import foto from '../images/foto.png';
 import translation from '../translations/AboutMeSection';
+import database from '../firebase/firebase';
 
-const AboutMeSection = (props) => {
-    return (
-        <div id="aboutMe" className="section">
-            <div className="content-container">
-                <div className="section__header">
-                    <h2>{translation[props.lang].section}</h2>
-                </div>
-                <div className="row about-me__content">
-                    <div className="about-me__photo center">
-                        <img src={foto} alt="me" />
+class AboutMeSection extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            profile: null
+        };
+        this.fetchProfile();
+    }
+    /**
+     * Fetch the information of my profile
+     */
+    fetchProfile() {
+        database.ref('/profile').once('value').then((snapshot) => {
+            const profile = snapshot.val();
+            this.setState({ profile });
+        })
+    }
+    render() {
+        return (
+            <div id="aboutMe" className="section">
+                <div className="content-container">
+                    <div className="section__header">
+                        <h2>{translation[this.props.lang].section}</h2>
                     </div>
-                    <div className="about-me__bio center">
-                       <p>
-                            {translation[props.lang].bio}
-                       </p>
+                    <div className="row about-me__content">
+                        <div className="about-me__photo center">
+                            {this.state.profile && <img src={this.state.profile.photo.link} alt="me" />}
+                        </div>
+                        <div className="about-me__bio center">
+                            {
+                                this.state.profile &&  
+                                <p>
+                                    {this.state.profile.bio}
+                                </p>
+                            }
+                        </div>
                     </div>
-                </div>
-                <div className="subsection">
-                    <div className="subsection__header">
-                        <h3>Skills</h3>
-                    </div>
-                    <div className="row evenly-distributed skills">
-                        <SkillBox icon={faAngular} level={5} />
-                        <SkillBox icon={faReact} level={4} />
-                        <SkillBox icon={faNode} level={5} />
-                        <SkillBox icon={faPython} level={4} />
-                    </div>
-                    <div className="row evenly-distributed skills">
-                        <SkillBox icon={faHtml5} level={5} />
-                        <SkillBox icon={faSass} level={3} />
-                        <SkillBox icon={faGitAlt} level={4} />
+                    <div className="subsection">
+                        <div className="subsection__header">
+                            <h3>Skills</h3>
+                        </div>
+                        <div className="row evenly-distributed skills">
+                            <SkillBox icon={faAngular} level={5} />
+                            <SkillBox icon={faReact} level={4} />
+                            <SkillBox icon={faNode} level={5} />
+                            <SkillBox icon={faPython} level={4} />
+                        </div>
+                        <div className="row evenly-distributed skills">
+                            <SkillBox icon={faHtml5} level={5} />
+                            <SkillBox icon={faSass} level={3} />
+                            <SkillBox icon={faGitAlt} level={4} />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default AboutMeSection
--- src/components/PortfolioSection.js
+++ src/components/PortfolioSection.js
@@ -1,21 +1,44 @@
 import React from 'react';
 import Project from './Project';
+import database from '../firebase/firebase';
+// import projects from '../fixtures/projects';
 
-import projects from '../fixtures/projects';
-
-const PortfolioSection = () => {
-    return (
-        <div id="portfolio" className="section">
-            <div className="content-container">
-                <div className="section__header">
-                    <h2>PORTFOLIO</h2>
+class PortfolioSection extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            projects: []
+        };
+        this.fetchProjects();
+    }
+    /**
+     * Method to fetch the projects from firebase
+     */
+    fetchProjects = () => {
+        database.ref('/projects').once('value').then((snapshot) => {
+            const projects = [];
+            snapshot.forEach((childSnapshot) => {
+                projects.push({
+                    ...childSnapshot.val()
+                })
+            })
+            this.setState({ projects });
+        })
+    }
+    render() {
+        return (
+            <div id="portfolio" className="section">
+                <div className="content-container">
+                    <div className="section__header">
+                        <h2>PORTFOLIO</h2>
+                    </div>
+                    {
+                        this.state.projects.map((project, idx) => (<Project project={project} key={`project-${idx}`} />))
+                    }
                 </div>
-                {
-                    projects.map((project, idx) => (<Project project={project} key={`project-${idx}`} />))
-                }
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default PortfolioSection;
