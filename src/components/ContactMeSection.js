e9175fb45ef3faadbe1cca1606cc194fe6760c0e
Daniel Rodriguez <danielrs9705@gmail.com>
2020-06-09 12:50:44 +0200

    Delete version number

--- src/components/ContactMeSection.js
+++ src/components/ContactMeSection.js
@@ -41,7 +41,7 @@ const ContactMeSection = () => {
                 </div>
                 <div className="footer__app-versioning">
                     <p>
-                        Portfolio app, Version 1.2. May 2020
+                        Portfolio app. May 2020
                     </p>
                 </div>
             </div>
